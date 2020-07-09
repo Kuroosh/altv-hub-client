65e679f9dbd2c86b7871a137383c9e73db5daf2e
Trevor Wessel <stuykgaming@gmail.com>
2020-07-09 14:16:41 -0600

    Deploy

---
+++ precache-manifest.0f9e24bed6c824325fa7aca17b51587e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8754badd0ba4b7fb57",
+    "url": "css/app.32cd50d1.css"
+  },
+  {
+    "revision": "1268d62960ba5a2d5209dbbdc3834cc5",
+    "url": "img/background.1268d629.png"
+  },
+  {
+    "revision": "e40030420bc15132f3d45d9be6370b86",
+    "url": "img/logo.e4003042.svg"
+  },
+  {
+    "revision": "ebda97eef992b99873a9b3b750c22936",
+    "url": "index.html"
+  },
+  {
+    "revision": "0e8754badd0ba4b7fb57",
+    "url": "js/app.e31f8d5c.js"
+  },
+  {
+    "revision": "036fbb8ecea87567d9aa",
+    "url": "js/chunk-vendors.110a1bc2.js"
+  },
+  {
+    "revision": "664ce1e7799651861e0d0849ad7513ab",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
